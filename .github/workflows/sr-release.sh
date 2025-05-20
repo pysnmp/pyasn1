af52927e22b2b788c038ab085d8f1ead2f7e804b
Ryan Faircloth <ryan@dss-i.com>
2025-05-20 01:37:19 -0500

    ci: update dusty ci

--- .github/workflows/sr-release.sh
+++ .github/workflows/sr-release.sh
@@ -1,6 +1,8 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 
 set -eE
 set -v
-echo pypy user=${PYPI_USERNAME}
-poetry publish -n -u ${PYPI_USERNAME} -p ${PYPI_TOKEN}
+echo "Publishing to PyPI as user: ${PYPI_USERNAME}"
+# Use poetry's modern auth approach with token
+poetry config pypi-token.pypi ${PYPI_TOKEN}
+poetry publish -n
